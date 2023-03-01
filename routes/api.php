1302c82a369b421ae53a8dc708683ba09898ad57
Vitalii Lyskov <vitalii.lyskov@onix-systems.com>
2023-03-01 17:21:58 +0200

    feat/implemented email verification

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\VerificationController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,3 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::apiResource('users', UserController::class);
+
+Route::middleware('auth')->group(function () {
+    Route::get('/email/verify/{id}/{hash}', [VerificationController::class, 'verifyEmail'])->name('verification.verify');
+    Route::get('/email/verify/{user}', [VerificationController::class, 'resendVerificationEmail'])->name('verification.resend')->middleware('throttle:6,1');
+});
